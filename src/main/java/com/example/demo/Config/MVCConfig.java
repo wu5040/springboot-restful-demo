eb7b30eb9e997debc6bb6f402cb403024d5f2ba2
wu5040 <wu5040@gmail.com>
2019-05-05 10:30:49 +0800

    Cors

--- src/main/java/com/example/demo/Config/MVCConfig.java
+++ src/main/java/com/example/demo/Config/MVCConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,21 @@ public class MVCConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**"); // 拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOrigins("*")
+                //是否发送Cookie信息
+                .allowCredentials(true)
+                //放行哪些原始域(请求方式)
+                .allowedMethods("GET","POST", "PUT", "DELETE")
+                //放行哪些原始域(头部信息)
+                .allowedHeaders("*")
+                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                .exposedHeaders("Header1", "Header2");
+    }
 
     @Bean
     public AuthenticationInterceptor authenticationInterceptor(){
